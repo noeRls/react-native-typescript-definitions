16ea4810a6a829eec241ad13b17321e63b1b903a
Noe <noe@kraaft.co>
2021-11-17 14:22:26 +0100

    Add target in LayoutChangeEvent type

--- typings/react-native/react-native.d.ts
+++ typings/react-native/react-native.d.ts
@@ -676,6 +676,7 @@ declare namespace  __React {
     export interface LayoutChangeEvent {
         nativeEvent: {
             layout: LayoutRectangle
+            target: number | null | undefined
         }
     }
 
